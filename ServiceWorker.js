7d07066114536e8cf779ccdc4f63740a0c5f88fa
島田 顕生 <akimi@Akimis-iMac.local>
2019-12-23 10:12:50 +0900

    WebFramework added

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,1 +1,1 @@
-importScripts("SW/SW.js");cacheManager({name:"Library",build:[2019,12,20],list:["Color/Color.js","Math/Math.js","SW/SW.js"]});
+importScripts("SW/SW.js"); cacheManager({ name: "Library", build: [2019, 12, 23], list: ["Color/Color.js", "Math/Math.js", "SW/SW.js", "WebFramework/WF.js"] });
